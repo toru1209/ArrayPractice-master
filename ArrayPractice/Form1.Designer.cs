b67ea7db049e574d53b793e16684ae0ecc900c1d
A601-20 <tnk.dat.class@gmail.com>
2021-06-24 09:52:49 +0900

    跳かえり

--- ArrayPractice/Form1.Designer.cs
+++ ArrayPractice/Form1.Designer.cs
@@ -33,6 +33,10 @@ namespace ArrayPractice
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.scoreLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // label1
@@ -45,6 +49,7 @@ namespace ArrayPractice
             this.label1.Size = new System.Drawing.Size(67, 30);
             this.label1.TabIndex = 0;
             this.label1.Text = "(・ω・)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // timer1
             // 
@@ -63,11 +68,45 @@ namespace ArrayPractice
             this.scoreLabel.TabIndex = 1;
             this.scoreLabel.Text = "Score 0";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(306, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "(・ω・)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(69, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 30);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "(・ω・)";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 50;
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 50;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(464, 281);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.scoreLabel);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -82,6 +121,10 @@ namespace ArrayPractice
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
     }
 }
 
--- ArrayPractice/Form1.cs
+++ ArrayPractice/Form1.cs
@@ -17,6 +17,12 @@ namespace ArrayPractice
         int vx = rand.Next(-20, 21);
         int vy = rand.Next(-20, 21);
         int score = 100;
+        int vx2 = rand.Next(-20, 21);
+        int vy2 = rand.Next(-20, 21);
+        int score2 = 100;
+        int vx3 = rand.Next(-20, 21);
+        int vy3 = rand.Next(-20, 21);
+        int score3 = 100;
 
         public Form1()
         {
@@ -24,6 +30,9 @@ namespace ArrayPractice
 
             label1.Left = rand.Next(ClientSize.Width - label1.Width);
             label1.Top = rand.Next(ClientSize.Height - label1.Height);
+
+            label2.Left = rand.Next(ClientSize.Width - label2.Width);
+            label2.Top = rand.Next(ClientSize.Height - label2.Height);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -33,6 +42,10 @@ namespace ArrayPractice
 
             label1.Left += vx;
             label1.Top += vy;
+            label2.Left += vx2;
+            label2.Top += vy2; 
+            label3.Left += vx3;
+            label3.Top += vy3;
 
             if (label1.Left < 0)
             {
@@ -51,6 +64,40 @@ namespace ArrayPractice
                 vy = -Math.Abs(vy);
             }
 
+            if (label2.Left < 0)
+            {
+                vx2 = Math.Abs(vx);
+            }
+            if (label2.Top < 0)
+            {
+                vy2 = Math.Abs(vy);
+            }
+            if (label2.Right > ClientSize.Width)
+            {
+                vx2 = -Math.Abs(vx);
+            }
+            if (label2.Bottom > ClientSize.Height)
+            {
+                vy2 = -Math.Abs(vy);
+            }
+
+            if (label3.Left < 0)
+            {
+                vx3 = Math.Abs(vx);
+            }
+            if (label3.Top < 0)
+            {
+                vy3 = Math.Abs(vy);
+            }
+            if (label3.Right > ClientSize.Width)
+            {
+                vx3 = -Math.Abs(vx);
+            }
+            if (label3.Bottom > ClientSize.Height)
+            {
+                vy3 = -Math.Abs(vy);
+            }
+
             Point fpos = PointToClient(MousePosition);
 
             if ((fpos.X >= label1.Left)
@@ -60,6 +107,12 @@ namespace ArrayPractice
             {
                 timer1.Enabled = false;
             }
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
